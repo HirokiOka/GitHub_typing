c0489a5b1c4cab2b19707c552916d26c8ac9b6a5
hiroki <hiro_bass.0827@icloud.com>
2019-07-30 14:54:09 +0900

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,121 @@
+'use strict'
+{
+    const target = document.getElementById('target');
+    const typed = document.querySelector('#typed');
+    const code = document.querySelector('#code');
+    const pre = document.querySelector("#pre")
+    const scoreLabel = document.getElementById('score');
+    const missLabel = document.getElementById('miss');
+    const langLabel = document.getElementById('lang');
+    let socket = io.connect('https://fathomless-badlands-48109.herokuapp.com/');
+
+    let word;
+    let loc;
+    let score;
+    let miss;
+    let lang;
+    let isPlaying = false;
+    let wait = true;
+    let audioElem;
+
+    function playKeySound() {
+        audioElem = new Audio();
+        audioElem.src = "../sound/key.mp3";
+        audioElem.play();
+    }
+
+    function playMissSound() {
+        audioElem = new Audio();
+        audioElem.src = "../sound/miss.mp3";
+        audioElem.play();
+    }
+
+    function playClearSound() {
+        audioElem = new Audio();
+        audioElem.src = "../sound/clear.mp3";
+        audioElem.play();
+    }
+
+    function updateTarget2() {
+        typed.textContent = word.substring(0, loc);
+        code.textContent = word.substring(loc, word.length);
+        playKeySound();
+        Prism.highlightAll();
+    }
+
+    socket.on('getCode', function(getCode) {
+        word = getCode;
+    });
+
+    socket.on('getLang', function(getLang) {
+        lang = getLang;
+        pre.classList.add('language-' + lang);
+        code.classList.add('language-' + lang);
+    });
+
+    setTimeout(function() {
+        target.textContent = 'click to start';
+        wait = false;
+    }, 3500);
+
+    console.log("ガバガバだけどチートしないでね");
+
+    window.addEventListener('click', () => {
+
+        if ((isPlaying === true) || (wait === true)) {
+            return;
+        }
+        isPlaying = true;
+        target.textContent = '';
+
+        loc = 0;
+        score = 0;
+        miss = 0;
+        scoreLabel.textContent = score;
+        missLabel.textContent = miss;
+        langLabel.textContent = 'lang:' + lang;
+
+        updateTarget2();
+        
+    });
+    
+    window.addEventListener('keydown', e => {
+        if (isPlaying !== true) {
+            return;
+        }
+
+        while((word[loc] === '\n') || (word[loc] === ' ') || (word[loc] === '\t')) {
+                    loc++;
+        }
+            
+        if(e.key === word[loc]){
+            loc++;
+            if (loc === word.length) {
+                playClearSound();
+                target.textContent = 'clear!';
+                code.textContent = '';
+                isPlaying = false;
+            }
+            score++;
+            scoreLabel.textContent = score;
+            
+            updateTarget2();
+            
+        } else {
+            if (e.keyCode !==  16) {
+                playMissSound();
+                miss++;
+                missLabel.textContent = miss;
+            }
+        }
+
+    });
+
+    window.addEventListener('keydown', function(e) {
+        if(e.keyCode == 32 && e.target == document.body) {
+          e.preventDefault();
+        }
+      });
+    
+
+}
---
+++ server.js
@@ -0,0 +1,76 @@
+'use strict';
+{
+
+    const GITHUB_ACCESS_TOKEN = 'a0169dbb54724a1514d78427aa0356048d2986bf';
+    const getOptions = {
+        'method': 'get',
+        'contentType': 'application/json',
+        'headers': {
+          'Authorization': 'Bearer ' + GITHUB_ACCESS_TOKEN,
+          'User-Agent': 'request'
+        }
+    };
+    const express =require('express');
+    const app = express();
+    const http = require('http').Server(app);
+    const io = require('socket.io')(http);
+    const PORT = process.env.PORT || 7000;
+    const fetch = require('node-fetch');
+
+    let code;
+    let lang;
+
+    function getPublicGists() {
+        const url = 'https://api.github.com/gists/public?page=' + Math.floor(Math.random() * 10) +'&per_page=100';
+        fetch(url, getOptions)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                let filename = '';
+                let selectedGists = [];
+                let langList = [];
+                let language;
+
+                for (let i = 0; i < json.length; i++) {
+                    let gists = json[i].files;
+                    for (let value in gists) {
+                        filename = value;
+                    }
+                    language = json[i].files[filename].language;
+
+                    if ((language !== 'Markdown') && (language !== 'null') && (language !== 'Text') && (language !== 'JSON') && (language !== 'Jupyter Notebook')&& (language !== 'TeX') && (language !== undefined) && (language !== null)){
+                        selectedGists.push(json[i].files[filename].raw_url);
+                        langList.push(language);
+                    }
+                }
+                let index = Math.floor(Math.random() * selectedGists.length);
+                lang = langList[index].toLowerCase();
+                return selectedGists[index];
+            })
+            .then(function(rawUrl) {
+                fetch(rawUrl)
+                    .then((response) => response.text())
+                    .then((text) => code = text);
+            });
+    }
+
+    app.use(express.static(__dirname + '/public'));
+    app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/index.html');
+    });
+
+    io.on('connection', function(socket) {
+        //console.log('connected');
+        getPublicGists();
+        setTimeout(function() {
+            socket.emit('getCode', code);
+            socket.emit('getLang', lang);
+        }, 3500);
+        
+    });
+
+    http.listen(PORT, function() {
+        console.log('server listening. Port:' + PORT);
+    });
+}
