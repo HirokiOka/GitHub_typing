08da781f21d72dfc73e9af467d135ed002b72e4f
hiroki <hiro_bass.0827@icloud.com>
2019-07-30 15:40:14 +0900

    url fixed

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,7 @@
     const scoreLabel = document.getElementById('score');
     const missLabel = document.getElementById('miss');
     const langLabel = document.getElementById('lang');
-    let socket = io.connect('https://fathomless-badlands-48109.herokuapp.com/');
+    let socket = io.connect('https://calm-chamber-39150.herokuapp.com/');
     //let socket = io.connect('http://localhost:7000');
 
     let word;
